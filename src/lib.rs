d3922e9fbcf8f78b8d8afc0bfb6ffe5a8e69fb5f
Darshan Baral <darshanbaral@gmail.com>
2025-03-06 20:44:18 -0800

    modify list

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn modify_list(input_list: Vec<f64>) -> PyResult<Vec<f64>> {
+    let mut result_list: Vec<f64> = Vec::new();
+    let mut previous_value: Option<f64> = None;
+
+    for value in input_list.iter() {
+        if previous_value.is_none() {
+            result_list.push(value + 5.0);
+        } else {
+            if previous_value.unwrap() > 3.5 {
+                result_list.push(value + 3.0);
+            } else {
+                result_list.push(value + 5.0);
+            }
+        }
+        previous_value = Some(*value);
+    }
+
+    Ok(result_list)
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn resop(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(modify_list, m)?)?;
     Ok(())
 }
