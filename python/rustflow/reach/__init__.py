09d3f0377e50806e2f50200e5acb4dc3ff9bc282
Darshan Baral <darshanbaral@gmail.com>
2025-03-15 14:13:37 -0700

    re-organize and use python directory

---
+++ python/rustflow/reach/__init__.py
@@ -0,0 +1,4 @@
+from ..rustflow import reach
+
+# Now you can expose the Rust functions under this submodule
+muskingum_routing = reach.muskingum_routing
--- src_py/test_muskingum_routing.py
+++ src_py/test_muskingum_routing.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
-from rustflow import reach
+from rustflow.reach import muskingum_routing as mkr
 import pandas
 
 if __name__ == "__main__":
     df = pandas.read_csv("../data/gage_flow_data.csv", index_col=0, parse_dates=True)
     inflow = df["Flow (cfs)"]
-    outflow = reach.muskingum_routing(inflow=inflow,
+    outflow = mkr(inflow=inflow,
                   k=timedelta(hours=1),
                   x=0.25,
                   time_step=timedelta(minutes=15),
