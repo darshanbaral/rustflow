b8ab957a20b9a92cabcad2bcafb3a299ae9224b6
Darshan Baral <darshanbaral@gmail.com>
2025-03-15 12:49:29 -0700

    add as submodule

--- src_py/test_muskingum_routing.py
+++ src_py/test_muskingum_routing.py
@@ -1,14 +1,14 @@
 from datetime import timedelta
-from rustflow import muskingum_routing
+from rustflow import reach
 import pandas
 
 if __name__ == "__main__":
     df = pandas.read_csv("../data/gage_flow_data.csv", index_col=0, parse_dates=True)
-    inflow = df["Flow (cfs)"].to_list()
-    outflow = muskingum_routing(inflow=inflow,
-                                k=timedelta(hours=1),
-                                x=0.25,
-                                time_step=timedelta(minutes=15),
-                                sub_reaches=12)
+    inflow = df["Flow (cfs)"]
+    outflow = reach.muskingum_routing(inflow=inflow,
+                  k=timedelta(hours=1),
+                  x=0.25,
+                  time_step=timedelta(minutes=15),
+                  sub_reaches=12)
     o_df = pandas.DataFrame({"flow": outflow}, index=df.index)
     o_df.to_csv("../data/outflow.csv")
