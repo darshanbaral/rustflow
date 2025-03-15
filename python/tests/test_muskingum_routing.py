7a8e48679457cffe6de7f2ba532726189a73e908
Darshan Baral <darshanbaral@gmail.com>
2025-03-15 14:16:06 -0700

    move tests

--- python/tests/test_muskingum_routing.py
+++ python/tests/test_muskingum_routing.py
@@ -3,7 +3,7 @@ from rustflow.reach import muskingum_routing as mkr
 import pandas
 
 if __name__ == "__main__":
-    df = pandas.read_csv("../data/gage_flow_data.csv", index_col=0, parse_dates=True)
+    df = pandas.read_csv("./data/gage_flow_data.csv", index_col=0, parse_dates=True)
     inflow = df["Flow (cfs)"]
     outflow = mkr(inflow=inflow,
                   k=timedelta(hours=1),
@@ -11,4 +11,4 @@ if __name__ == "__main__":
                   time_step=timedelta(minutes=15),
                   sub_reaches=12)
     o_df = pandas.DataFrame({"flow": outflow}, index=df.index)
-    o_df.to_csv("../data/outflow.csv")
+    o_df.to_csv("./data/outflow.csv")
