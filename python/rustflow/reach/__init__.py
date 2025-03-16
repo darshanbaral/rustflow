f9db2dfce3a38e6040938f5b27bba715f141474b
Darshan Baral <darshanbaral@gmail.com>
2025-03-16 13:35:26 -0700

    use ruff

--- python/rustflow/reach/__init__.py
+++ python/rustflow/reach/__init__.py
@@ -4,10 +4,14 @@ from datetime import timedelta
 
 
 # Expose Rust functions
-def muskingum_routing(inflow: list[float],
-                      k: timedelta,
-                      x: float,
-                      time_step: timedelta,
-                      sub_reaches: Optional[int] = 1,
-                      initial_outflow: Optional[float] = None, ):
-    return reach.muskingum_routing(inflow, k, x, time_step, sub_reaches, initial_outflow)
+def muskingum_routing(
+    inflow: list[float],
+    k: timedelta,
+    x: float,
+    time_step: timedelta,
+    sub_reaches: Optional[int] = 1,
+    initial_outflow: Optional[float] = None,
+):
+    return reach.muskingum_routing(
+        inflow, k, x, time_step, sub_reaches, initial_outflow
+    )
